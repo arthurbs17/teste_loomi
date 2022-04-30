840c29494c4a0d7b750e041e9b732ca1f3514ba5
Arthur Sarmento <arthurbs17@gmail.com>
2022-04-29 22:32:03 -0300

    feat: middleware validate shape apply

--- src/routes/cliente.routes.ts
+++ src/routes/cliente.routes.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import { CreateClienteController } from "../modules/cliente/useCases/CreateCliente.controller";
 import { GetClienteByIdController } from "../modules/cliente/useCases/GetCliente.controller";
+import { validateShape } from "../middlewares/validateShape.middleware";
+import { createClienteSchema } from "../schemas/createCliente.schema";
 
 const clienteRouter = Router();
 
 const createClienteController = new CreateClienteController();
 const getClienteByIdController = new GetClienteByIdController();
 
-clienteRouter.post("/register", createClienteController.handle);
+clienteRouter.post(
+  "/register",
+  validateShape(createClienteSchema),
+  createClienteController.handle
+);
 clienteRouter.get("/:cliente_id", getClienteByIdController.handle);
 
 export { clienteRouter };
