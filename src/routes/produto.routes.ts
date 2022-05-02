5331ead44b828798323902177a10307ada706a4a
Arthur Sarmento <arthurbs17@gmail.com>
2022-05-02 12:05:54 -0300

    feat: delete functions done

--- src/routes/produto.routes.ts
+++ src/routes/produto.routes.ts
@@ -3,6 +3,8 @@ import { authenticateToken } from "../middlewares/authenticateToken.middleware";
 import { validateShape } from "../middlewares/validateShape.middleware";
 import { verifyIfIsCliente } from "../middlewares/verifyIfIsCliente.middleware";
 import { CreateProdutoController } from "../modules/produto/useCases/CreateProduto.controller";
+import { DeleteProdutoController } from "../modules/produto/useCases/DeleteProduto.controller";
+import { DeleteProdutoByIdController } from "../modules/produto/useCases/DeleteProdutoById.controller";
 import { GetProdutoByIdController } from "../modules/produto/useCases/GetProdutoById.controller";
 import { GetProdutoWithParamsController } from "../modules/produto/useCases/GetProdutoWithParams.controller";
 import { createProdutoSchema } from "../schemas/createProduto.schema";
@@ -12,6 +14,8 @@ const produtoRouter = Router();
 const createProdutoController = new CreateProdutoController();
 const getProdutoWithParamsController = new GetProdutoWithParamsController();
 const getProdutoByIdController = new GetProdutoByIdController();
+const deleteProdutoController = new DeleteProdutoController();
+const deleteProdutoByIdController = new DeleteProdutoByIdController();
 
 produtoRouter.post(
   "/register",
@@ -31,6 +35,20 @@ produtoRouter.get(
   "/:produto_id",
   authenticateToken,
   getProdutoByIdController.handle
+);
+
+produtoRouter.delete(
+  "/delete",
+  authenticateToken,
+  verifyIfIsCliente,
+  deleteProdutoController.handle
+);
+
+produtoRouter.delete(
+  "/delete/:produto_id",
+  authenticateToken,
+  verifyIfIsCliente,
+  deleteProdutoByIdController.handle
 );
 
 export { produtoRouter };
