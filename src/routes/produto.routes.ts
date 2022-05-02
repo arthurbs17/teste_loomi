5dd40e50fed7fcfc9b1042218d6ebf6342f5b784
Arthur Sarmento <arthurbs17@gmail.com>
2022-05-02 11:35:12 -0300

    feat: functionality get produtos done

--- src/routes/produto.routes.ts
+++ src/routes/produto.routes.ts
@@ -3,11 +3,15 @@ import { authenticateToken } from "../middlewares/authenticateToken.middleware";
 import { validateShape } from "../middlewares/validateShape.middleware";
 import { verifyIfIsCliente } from "../middlewares/verifyIfIsCliente.middleware";
 import { CreateProdutoController } from "../modules/produto/useCases/CreateProduto.controller";
+import { GetProdutoByIdController } from "../modules/produto/useCases/GetProdutoById.controller";
+import { GetProdutoWithParamsController } from "../modules/produto/useCases/GetProdutoWithParams.controller";
 import { createProdutoSchema } from "../schemas/createProduto.schema";
 
 const produtoRouter = Router();
 
 const createProdutoController = new CreateProdutoController();
+const getProdutoWithParamsController = new GetProdutoWithParamsController();
+const getProdutoByIdController = new GetProdutoByIdController();
 
 produtoRouter.post(
   "/register",
@@ -15,6 +19,18 @@ produtoRouter.post(
   authenticateToken,
   verifyIfIsCliente,
   createProdutoController.handle
+);
+
+produtoRouter.get(
+  "/",
+  authenticateToken,
+  getProdutoWithParamsController.handle
+);
+
+produtoRouter.get(
+  "/:produto_id",
+  authenticateToken,
+  getProdutoByIdController.handle
 );
 
 export { produtoRouter };
